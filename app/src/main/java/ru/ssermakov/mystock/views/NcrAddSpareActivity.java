5ffd068005ae5a3edae3194a3c5b54947ce3cdcd
Ermakov <btbwild.ermakov@gmail.com>
2019-03-04 00:18:08 +1200

    open and iterate .xls file

--- app/src/main/java/ru/ssermakov/mystock/views/NcrAddSpareActivity.java
+++ app/src/main/java/ru/ssermakov/mystock/views/NcrAddSpareActivity.java
@@ -1,10 +1,40 @@
 package ru.ssermakov.mystock.views;
 
+import android.Manifest;
+import android.content.ContentUris;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.provider.OpenableColumns;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Iterator;
 
 import ru.ssermakov.mystock.R;
 import ru.ssermakov.mystock.controllers.NcrAddSpareController;
@@ -12,8 +42,11 @@ import ru.ssermakov.mystock.data.room.entity.Spare;
 
 public class NcrAddSpareActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final int EXCEL_REQUEST_CODE = 10;
+    private static final String TAG = "EXCEL";
+    private static final int MY_PERMISSIONS_REQUEST_READ_MEDIA = 23;
     private EditText state, pn, desc, quantity, reworkCode;
-    Button addSpareButton;
+    Button addSpareButton, openExcelButton;
     NcrAddSpareController ncrAddSpareController;
 
 
@@ -31,8 +64,19 @@ public class NcrAddSpareActivity extends AppCompatActivity implements View.OnCli
         reworkCode = findViewById(R.id.reworkNcrAddSpareEditText);
 
         addSpareButton = findViewById(R.id.addNcrSpareButton);
+        openExcelButton = findViewById(R.id.openNcrExcelButton);
 
         addSpareButton.setOnClickListener(this);
+        openExcelButton.setOnClickListener(this);
+
+        // requesting permissions
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_MEDIA);
+        } else {
+//            Toast.makeText(this, "You have permissions", Toast.LENGTH_SHORT).show();
+        }
     }
 
 
@@ -55,5 +99,248 @@ public class NcrAddSpareActivity extends AppCompatActivity implements View.OnCli
                     quantity
             ));
         }
+
+        if (viewId == R.id.openNcrExcelButton) {
+            Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+            i.setType("application/vnd.ms-excel");
+//            i.setType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+            i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivityForResult(i, EXCEL_REQUEST_CODE);
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == EXCEL_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Uri selectedImage = data.getData();
+                String fileName = getFileName(selectedImage);
+                String filePath = null;
+                filePath = getPath(this, selectedImage);
+                File fileSource = new File(filePath);
+                readExcelFile(this, filePath);
+
+            }
+        }
+    }
+
+    private String getFileName(Uri selectedImage) {
+        String fileName = null;
+        if (selectedImage.getScheme().equals("content")) {
+            Cursor cursor = getContentResolver().query(
+                    selectedImage,
+                    null,
+                    null,
+                    null,
+                    null
+            );
+            if (cursor != null && cursor.moveToFirst()) {
+                try {
+                    fileName = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                } finally {
+                    cursor.close();
+                }
+            }
+            if (fileName == null) {
+
+                fileName = selectedImage.getPath();
+                int cut = fileName.lastIndexOf("/");
+                if (cut != -1) {
+                    fileName = fileName.substring(cut + 1);
+                }
+            }
+        }
+        return fileName;
+    }
+
+    public static String getPath(final Context context, final Uri uri) {
+
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+
+                // TODO handle non-primary volumes
+            }
+            // DownloadsProvider
+            else if (isDownloadsDocument(uri)) {
+
+                final String id = DocumentsContract.getDocumentId(uri);
+                if (!TextUtils.isEmpty(id)) {
+                    if (id.startsWith("raw:")) {
+                        return id.replaceFirst("raw:", "");
+                    }
+                    try {
+                        final Uri contentUri = ContentUris.withAppendedId(
+                                Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+                        return getDataColumn(context, contentUri, null, null);
+                    } catch (NumberFormatException e) {
+                        return null;
+                    }
+                }
+            }
+            // MediaProvider
+            else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[]{
+                        split[1]
+                };
+
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        }
+        // MediaStore (and general)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            return getDataColumn(context, uri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+
+        return null;
+    }
+
+    /**
+     * Get the value of the data column for this Uri. This is useful for
+     * MediaStore Uris, and other file-based ContentProviders.
+     *
+     * @param context       The context.
+     * @param uri           The Uri to query.
+     * @param selection     (Optional) Filter used in the query.
+     * @param selectionArgs (Optional) Selection arguments used in the query.
+     * @return The value of the _data column, which is typically a file path.
+     */
+    public static String getDataColumn(Context context, Uri uri, String selection,
+                                       String[] selectionArgs) {
+
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {
+                column
+        };
+
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+                    null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int column_index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(column_index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is ExternalStorageProvider.
+     */
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+
+    }
+
+    private static void readExcelFile(Context context, String filename) {
+
+//        if (!isExternalStorageAvailable() || isExternalStorageReadOnly())
+//        {
+//            Log.e(TAG, "Storage not available or read only");
+//            return;
+//        }
+
+        try {
+            // Creating Input Stream
+            File file = new File(filename);
+            FileInputStream myInput = new FileInputStream(file);
+
+            // Create a POIFSFileSystem object
+            POIFSFileSystem myFileSystem = new POIFSFileSystem(myInput);
+
+            // Create a workbook using the File System
+            HSSFWorkbook myWorkBook = new HSSFWorkbook(myFileSystem);
+
+            // Get the first sheet from workbook
+            HSSFSheet mySheet = myWorkBook.getSheetAt(0);
+
+            /** We now need something to iterate through the cells.**/
+            Iterator rowIter = mySheet.rowIterator();
+
+            while (rowIter.hasNext()) {
+                HSSFRow myRow = (HSSFRow) rowIter.next();
+                Iterator cellIter = myRow.cellIterator();
+                while (cellIter.hasNext()) {
+                    HSSFCell myCell = (HSSFCell) cellIter.next();
+                    if (myCell.getCellType() == CellType.NUMERIC) {
+                        myCell.setCellType(CellType.STRING);
+                    }
+                    Log.d(TAG, "Cell value: " + myCell.toString());
+                    Toast.makeText(context, "cell Value: " + myCell.toString(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return;
+    }
+
+    public static boolean isExternalStorageReadOnly() {
+        String extStorageState = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(extStorageState)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isExternalStorageAvailable() {
+        String extStorageState = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(extStorageState)) {
+            return true;
+        }
+        return false;
     }
 }
